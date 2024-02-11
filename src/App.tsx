4a2eb486ddc2343c1639eb54657f0285e81b893c
cyberDaffodil <cyberDaffodil@proton.me>
2024-02-11 01:58:09 -0500

    fade in quotes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import { ArrowRight } from 'lucide-solid';
-import { Setter, Show, createSignal, type Component } from 'solid-js';
+import {
+  For,
+  Setter,
+  Show,
+  createEffect,
+  createResource,
+  createSignal,
+  type Component,
+} from 'solid-js';
 import { Motion, Presence } from 'solid-motionone';
 import { Cursor } from './lib/typewriter/components';
 import { createTypeWriter } from './lib/typewriter/createTypeWriter';
 
 type Screen = 'DECIDE' | 'QUOTES';
+
 const App: Component = () => {
   const [screen, setScreen] = createSignal<Screen>('DECIDE');
 
@@ -66,21 +75,92 @@ const Decide: Component<Slide> = (props) => {
 };
 
 const Quotes: Component<Slide> = (props) => {
+  const fetchQuotes = async () => {
+    try {
+      const response = await fetch('../data/quotes.json');
+      if (!response.ok) {
+        throw new Error('Failed to fetch quotes');
+      }
+      const quotes: Quote[] = await response.json();
+      return quotes;
+      // Do something with the quotes
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  };
+
+  const [data] = createResource(fetchQuotes);
+
   return (
     <Motion
       animate={{ height: [0, '100%'] }}
       transition={{ delay: 4.6, duration: 3 }}
+      class='py-6'
     >
       <Motion.section
         animate={{ opacity: [0, 1] }}
         transition={{ delay: 0.8, easing: 'ease-in', duration: 1.5 }}
       >
-        <span class='text-stone-400 md:text-2xl text-lg'>
+        <span class='text-stone-400 md:text-2xl text-center'>
           The following are real quotes from Israeli state officials and
           military leaders
         </span>
       </Motion.section>
+      <Motion.section
+        animate={{ opacity: [0, 1] }}
+        transition={{ delay: 8, duration: 1 }}
+        class='h-full space-y-10 py-4'
+      >
+        <For each={data()}>{(item, index) => <PersonCard {...item} />}</For>
+      </Motion.section>
     </Motion>
+  );
+};
+
+type Quote = {
+  quote: string;
+  'person-name': string;
+  'person-title': string;
+  'person-url': string;
+  'image-url': string;
+  'source-url': string;
+};
+const PersonCard: Component<Quote> = (props) => {
+  const [isVisible, setVisible] = createSignal(false);
+  let elRef: HTMLDivElement | null;
+
+  createEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => setVisible(entry.isIntersecting));
+    });
+    observer.observe(elRef);
+  });
+
+  return (
+    <div
+      class={`flex gap-6 fade-in-section ${isVisible() ? 'is-visible' : ''}`}
+      ref={elRef}
+    >
+      <img
+        src={props['image-url']}
+        alt={props['person-name']}
+        class={`md:w-20 md:h-28 w-12 h-20 blur-sm  `}
+        style={{
+          'object-fit': 'cover',
+          filter: 'grayscale(100%)',
+        }}
+      />
+      <blockquote
+        cite={props['source-url']}
+        class='text-stone-400 md:text-lg text-xs font-thin font-sans'
+      >
+        {props.quote}
+        <cite class='md:text-lg text-xs'>
+          {props['person-name']}, {props['person-title']}
+        </cite>
+      </blockquote>
+    </div>
   );
 };
 
