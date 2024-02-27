0fe016a019679b684fbba50f7f882d0ef5aeec8e
cyberDaffodil <cyberDaffodil@proton.me>
2024-02-26 21:23:06 -0500

    change intro animation typerwriter

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -1,7 +1,6 @@
-import { Cursor } from "../lib/typewriter/components";
-import { createTypeWriter } from "../lib/typewriter/createTypeWriter";
+import Typewriter from "typewriter-effect/dist/core";
 import { ArrowRight } from "lucide-solid";
-import { Setter, Component, createEffect } from "solid-js";
+import { Setter, Component, onMount } from "solid-js";
 import { Motion } from "solid-motionone";
 
 export type Screen = "DECIDE" | "QUOTES";
@@ -11,53 +10,43 @@ interface Slide {
 }
 
 export const Decide: Component<Slide> = (props) => {
-  const typeWriter = createTypeWriter({
-    words: [
-      "The war in Gaza has been called a genocide...",
-      "Some say the allegation is anti-semitic. Is it a genocide?",
-    ],
-    typeSpeed: 50,
-    delaySpeed: 2000,
-    deleteSpeed: 30,
+  onMount(() => {
+    const t = new Typewriter(spanRef, {
+      delay: 50,
+      deleteSpeed: 30,
+    });
+    t.typeString("The war in Gaza has been called a genocide...")
+      .pauseFor(3000)
+      .deleteAll()
+      .typeString("Some say this allegation is anti-semitic.")
+      .pauseFor(1200)
+      .typeString(" But is it a <span class='color-anim'>genocide?</span>")
+      .start();
   });
+
   let spanRef: HTMLSpanElement | null;
-  let spanRef2: HTMLSpanElement | null;
-
-  const changeColor = () => {
-    if (spanRef) {
-      spanRef.remove();
-      spanRef2.innerHTML =
-        "Some say the allegation is anti-semitic. Is it a <span class='color-anim'>genocide?</span>";
-    }
-  };
-
-  // after 9s change color
-  createEffect(() => {
-    setTimeout(changeColor, 9000);
-  });
 
   return (
     <>
-      <section class="flex text-left text-stone-400 md:text-2xl text-lg md:h-40 h-32">
-        <span ref={spanRef}>{typeWriter().text}</span>
-        <span ref={spanRef2}></span>
-        <Cursor />
+      <section class="flex text-left text-stone-400 md:text-2xl text-xl md:h-40 h-32 relative">
+        <span ref={spanRef}></span>
       </section>
       <section class="flex items-center">
         <Motion.span
           animate={{ opacity: [0, 1] }}
-          transition={{ delay: 10, easing: "ease-in", duration: 1.5 }}
+          transition={{ delay: 16, easing: "ease-in", duration: 3.5 }}
           class="text-stone-400 md:text-2xl text-lg italic mr-4"
         >
           Decide for yourself
         </Motion.span>
         <Motion.button
           animate={{ opacity: [0, 1] }}
-          transition={{ delay: 10.5, easing: "ease-in", duration: 1.5 }}
-          class="bg-white/50 rounded-full p-1 hover:bg-white/60 duration-75 ease-in-out hover:shadow-sm hover:shadow-stone-300"
+          transition={{ delay: 16.5, easing: "ease-in", duration: 3.5 }}
+          class="bg-white/50 rounded-full p-1 hover:bg-white/60 duration-75 ease-in-out hover:shadow-sm hover:shadow-stone-300 relative animate-pulse"
           onclick={() => setTimeout(() => props.setScreen("QUOTES"), 200)}
         >
           <ArrowRight />
+          {/* <div class="rounded-full p-1 opacity-50 bg-white absolute top-0 right-0 animate-pulse"></div> */}
         </Motion.button>
       </section>
     </>
